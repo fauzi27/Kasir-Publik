1446f62bc0ccfa7d2167a4a8679bc88ac0a0833a
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-02-28 18:43:32 +0800

    update src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './css/style.css' // Memanggil Tailwind dan CSS kustom kita
 // Cek cache Dark Mode
 if (localStorage.getItem('darkMode') === 'true') {
-  document.body.classList.add('dark');
+  document.documentElement.classList.add('dark');
 }
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
